5af291d54fb997d0201f9da31c4b7e65f73364e5
Tom Gregor <tgregor@ivsoftware.com>
2023-11-30 11:00:38 -0700

    Working drawer

--- faux-shell-hamburger/MainPage.xaml.cs
+++ faux-shell-hamburger/MainPage.xaml.cs
@@ -1,13 +1,12 @@
-﻿namespace faux_shell_hamburger
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace faux_shell_hamburger
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+        public MainPage() => InitializeComponent();
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
@@ -20,10 +19,39 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
-
-        private void ShowFlyout(object sender, EventArgs e)
+        private async void ShowFlyout(object sender, EventArgs e)
         {
-
+            Overlay.IsVisible = true;
+            await Flyout.TranslateTo(0, 0, 250, Easing.SinInOut);
         }
+        private async void HideFlyout(object sender, EventArgs e)
+        {
+            await Flyout.TranslateTo(-250, 0, 250, Easing.SinInOut);
+            Overlay.IsVisible = false;
+        }
+
+        private void OnMenuItemTap(object sender, TappedEventArgs e)
+        {
+            if(e.Parameter is string text)
+            {
+                switch (text) 
+                {
+                    case "Flights":
+                        break;
+                    case "Beach Activities":
+                        break;
+                    case "Mail":
+                        break;
+                    case "Photos":
+                        break;
+                    case "Awards":
+                        break;
+                }
+            }
+            HideFlyout(sender, e);
+        }
+
+        private void OnOverlayTap(object sender, TappedEventArgs e) =>
+            HideFlyout(sender, e);
     }
 }
